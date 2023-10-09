04537b81f3f169d4579b6d42d1c4d6a79092ebfa
andrey.nascimento4@etec.sp.gov.br <andrey.nascimento4@etec.sp.gov.br>
2023-10-09 16:38:00 -0300

    tcc

--- login/jslogin/login.js
+++ login/jslogin/login.js
@@ -9,3 +9,9 @@ document.getElementById('open-login').addEventListener('click', moveOverlay)
 
 document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
 document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
+
+document.getElementById('celularInput').addEventListener('input', function () {
+    if (this.value.length > 11) {
+        this.value = this.value.slice(0,11); // Limita a 11 dígitos
+    }
+});
